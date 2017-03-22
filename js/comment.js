e483d70b1e0dfd5c0e668d9786734b1b616dd33a
MrXu <mrxu1010@outlook.com>
2017-03-22 19:12:43 +0800

    second 360WebApp

--- js/comment.js
+++ js/comment.js
@@ -1,6 +1,20 @@
 
 
-
+// var oHtml = document.documentElement;
+// getFont();
+// window.onresize = function () {
+//     getFont();
+// }
+// function getFont() {
+//     var screenWidth = oHtml.clientWidth;
+//     if (screenWidth <= 320) {
+//         oHtml.style.fontSize = '23.7037px';
+//     } else if (screenWidth >= 640) {
+//         oHtml.style.fontSize = '47.4074px';
+//     } else {
+//         oHtml.style.fontSize = screenWidth / (640 / 40) + 'px';
+//     }
+// }
 
 
 
