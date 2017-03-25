d1f2cc446688a9df55b7024c1120062a51f74e72
MrXu <mrxu1010@outlook.com>
2017-03-25 10:35:21 +0800

    fours 360webApp

--- js/comment.js
+++ js/comment.js
@@ -1,20 +1,20 @@
 
 
-// var oHtml = document.documentElement;
-// getFont();
-// window.onresize = function () {
-//     getFont();
-// }
-// function getFont() {
-//     var screenWidth = oHtml.clientWidth;
-//     if (screenWidth <= 320) {
-//         oHtml.style.fontSize = '23.7037px';
-//     } else if (screenWidth >= 640) {
-//         oHtml.style.fontSize = '47.4074px';
-//     } else {
-//         oHtml.style.fontSize = screenWidth / (640 / 40) + 'px';
-//     }
-// }
+var oHtml = document.documentElement;
+getFont();
+window.onresize = function () {
+    getFont();
+}
+function getFont() {
+    var screenWidth = oHtml.clientWidth;
+    if (screenWidth <= 320) {
+        oHtml.style.fontSize = '23.7037px';
+    } else if (screenWidth >= 640) {
+        oHtml.style.fontSize = '47.4074px';
+    } else {
+        oHtml.style.fontSize = screenWidth / (640 / 40) + 'px';
+    }
+}
 
 
 
@@ -23,15 +23,7 @@ mui.init();
 var offCanvasWrapper = mui('#offCanvasWrapper');
 //主界面容器
 var offCanvasInner = offCanvasWrapper[0].querySelector('.mui-inner-wrap');
-//菜单容器
-// var offCanvasSide = document.getElementById("offCanvasSide");
-// if (!mui.os.android) {
-//     document.getElementById("move-togger").classList.remove('mui-hidden');
-//     var spans = document.querySelectorAll('.android-only');
-//     for (var i = 0, len = spans.length; i < len; i++) {
-//         spans[i].style.display = "none";
-//     }
-// }
+
 //移动效果是否为整体移动
 var moveTogether = false;
 //侧滑容器的class列表，增加.mui-slide-in即可实现菜单移动、主界面不动的效果；
@@ -69,14 +61,7 @@ mui('.mui-input-group').on('change', 'input', function () {
         offCanvasWrapper.offCanvas().refresh();
     }
 });
-//主界面‘显示侧滑菜单’按钮的点击事件
-// document.getElementById('offCanvasShow').addEventListener('tap', function () {
-//     offCanvasWrapper.offCanvas('show');
-// });
-//菜单界面，‘关闭侧滑菜单’按钮的点击事件
-// document.getElementById('offCanvasHide').addEventListener('tap', function () {
-//     offCanvasWrapper.offCanvas('close');
-// });
+
 //主界面和侧滑菜单界面均支持区域滚动；
 mui('#offCanvasSideScroll').scroll();
 mui('#offCanvasContentScroll').scroll();
@@ -90,4 +75,31 @@ if (mui.os.plus && mui.os.ios) {
 }
 
 
+//tap为mui封装的单击事件，可参考手势事件章节
+document.getElementById('index').addEventListener('tap', function() {
+  //打开关于页面
+  mui.openWindow({
+    url: 'index.html', 
+    id:'index'
+  });
+});
+
+//tap为mui封装的单击事件，可参考手势事件章节
+document.getElementById('compare').addEventListener('tap', function() {
+  //打开关于页面
+  mui.openWindow({
+    url: 'compare.html', 
+    id:'compare'
+  });
+});
+
+
+//tap为mui封装的单击事件，可参考手势事件章节
+document.getElementById('detail').addEventListener('tap', function() {
+  //打开关于页面
+  mui.openWindow({
+    url: 'detail.html', 
+    id:'detail'
+  });
+});
 
