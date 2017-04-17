c9867262d48b62eb4c58f0f976e6c09ba403a9f8
MrXu <mrxu1010@outlook.com>
2017-04-17 18:47:12 +0800

    ti

--- js/definition.js
+++ js/definition.js
@@ -1,20 +1,29 @@
 $(function (argument) {
      $('.module-list').each(function () {
         var _this = $(this);
-        _this.on('click', function () {
+        _this.on('tap', function () {
             _this.next().toggle();
+            _this.toggleClass('mui-icon-arrowup mui-icon-arrowdown');
         });
+    })
+
+    $('.explain_list').each(function(){
+        var index = $(this).index();
+        $(this).on('tap', function(){
+            $('show_over').children('div').eq(index).toggle();
+            $(this).children('a').toggleClass('mui-icon-arrowright mui-icon-arrowdown');
+            $(this).children('div').addClass('explain_bd').parent().siblings().children('div').removeClass('explain_bd');
+        })
     })
 
     var uls = $('.ul');
     $('.list').each(function () {
-        console.log(uls);
-        $(this).on('click', '.study', function () {
+        $(this).on('tap', '.study', function () {
             $(this).next().toggle();
             $(this).toggleClass('mui-icon-arrowright mui-icon-arrowdown');
             $(this).addClass('active_color').parent().parent().parent().siblings().find('h4').removeClass('active_color');
         })
-        $(this).on('click', '.study', function () {
+        $(this).on('tap', '.study', function () {
             $(this).addClass('active_color').parent().parent().parent().parent().parent().siblings().children('ul').children('li').find('h4').removeClass('active_color');
         })
     })
